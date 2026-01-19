6e1f1cafab66b2c05416412b386f334323613b7f
Nadeem <nadeemansari45612@gmail.com>
2026-01-19 21:36:25 +0530

    It is main file of terraform

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.28.0"
+    
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "nadeem" {
+  ami = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+}
